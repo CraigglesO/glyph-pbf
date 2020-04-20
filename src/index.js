5e7d791f2c29cbe21004f555a8b00b7d77bbde48
CraigglesO <oconnorct1@gmail.com>
2020-04-19 21:33:59 -0400

    remove reference of opentype

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 // @flow
-export { default as buildFonts } from './buildFonts'
 export { default as GlyphSet } from './glyphSet'
 export { default as Glyph } from './glyph'
 export { default as serialize } from './serialize'
